c68aa0c85cfb25f2371fd57b1aa7f56d2567158a
Emmett McKinney <47829330+ezmckinn@users.noreply.github.com>
2022-12-21 09:40:51 -0800

    extending day 9 for part 2

--- 2022_12_09/day_9.py
+++ 2022_12_09/day_9.py
@@ -1,63 +1,69 @@
+
 file = open('input.txt', 'r') # read lines
 input = file.readlines()
 rows = list([row[:-1] for row in input])
-rope_length = 2
-rope = [[0,0] for x in range(0, rope_length)] 
+rope_length = 10 #update value to "2" for part 1 or "10" for part 2
+rope = [[0,0] for x in range(0, rope_length)] #build rope
 
-t_pos_list = [[0,0]]
-head = []
-tail = []
+t_pos_list = []
+
+"""
+helper function to define movement of each knlot in response to next one.
+"""
+def find_next(dif): 
+    if dif > 1:
+        return dif-1
+    elif dif < -1:
+        return dif+1
+    else:
+        return dif
 
 for row in rows:
     dir = row.split(' ')[0]
     dist = int(row.split(' ')[1])
-    print('Command: ' + str(row))
+    
     print('Starting Rope: ' + str(rope))
-    for x in range(0,dist):
-        ## main thing missing for part 2 is accounting for when dist != rope length. current syntax asusme whole rope moves every time.
-       
-        for n in range(0, rope_length-1):
-            
-                head.append(rope[n][:])
-                tail.append(rope[n+1][:])
-            
-                if dir == 'U':
-                    rope[n][1]+=1
-                elif dir == 'D':
-                    rope[n][1]-=1
-                elif dir == 'R':
-                    rope[n][0]+=1
-                elif dir == 'L':
-                    rope[n][0]-=1
-            
-                y_dif = abs(rope[n+1][1] - rope[n][1])
-                x_dif = abs(rope[n+1][0] - rope[n][0])
-                if ((x_dif >= 2) | (y_dif >= 2)):
-                    new_tail = rope[n+1][:]
-                    rope[n+1] = head[0]
+    print('Command: ' + str(row))
 
-                    """
-                    new_y_dif = abs(new_tail[1] - rope[n+1][1])
-                    new_x_dif = abs(new_tail[0] - rope[n+1][0])
-                    
-                    if ((new_y_dif >= 2) | (new_x_dif >= 2)):
-                        rope[n+1] = new_tail
-                    """   
+    for x in range(0,dist): 
+        #first, move the head
 
-                    print('New Tail: ' + str(rope[n+1]))
+        if dir == 'U':
+            rope[0][1]+=1
+        elif dir == 'D':
+            rope[0][1]-=1
+        elif dir == 'R':
+            rope[0][0]+=1
+        elif dir == 'L':
+            rope[0][0]-=1   
 
-                    if list(rope[n+1]) not in t_pos_list:
-                        t_pos_list.append(rope[n+1])
-                else:
-                    rope[n+1] = tail[0]
-                    print('No Tail Movement')
+        for n in range(1, rope_length): # next, loop through the tail
+            #measure initial difference between head and next knot
+            y_dif = rope[n-1][1] - rope[n][1]
+            x_dif = rope[n-1][0] - rope[n][0]
 
-                head.pop(0)
-                tail.pop(0)
-            
-           
+            if ((abs(x_dif) < 2) & (abs(y_dif) < 2)): #if any given knot is close to the next one, stop moving tail.
+                break
+            elif ((abs(x_dif) > 2) | (abs(y_dif) > 2)):
+                print('Error! Rope broken!') # if the head gets too far from the tail, the rope is broken.
+                break
+            else: 
+                #measure movement
+                next_y_dif = find_next(y_dif)
+                next_x_dif = find_next(x_dif)
+                #update position of this knot
+                rope[n][0]+=next_x_dif
+                rope[n][1]+=next_y_dif
+                
+        #once the whole rope has shifted, see if the tail is on a new spot.
 
-print('Answer: ' + str(len(t_pos_list)))   
+        if list(rope[-1]) not in t_pos_list:
+            t_pos_list.append(list(rope[-1]))                    
+
+#print number of unique tail values.
+print('Answer with rope of length {}: '.format(rope_length) + str(len(t_pos_list)))   
+
+
     
             
 
